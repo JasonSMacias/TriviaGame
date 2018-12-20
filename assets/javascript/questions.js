f4ce89450612ed65c7c6b891f64b910ffbd69bcb
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2018-12-19 21:24:16 -0500

    adde question/answer pairs as objects in their own js file

---
+++ assets/javascript/questions.js
@@ -0,0 +1,72 @@
+// sci-fi clues and answers from jeapordy database, with wrong answers added by me.
+
+var sfq1 = {
+  clue:'The novel "Do Androids Dream of Electric Sheep?" inspired this Harrison Ford film.',
+  answer:'<em>Blade Runner</em>',
+  wrongAnswer1:'<em>Liquid Sky</em>',
+  wrongAnswer2:'<em>Star Wars</em>',
+  wrongAnswer3:'<em>The Fugitive</em>',
+}
+var sfq2 = {
+  clue:'"The War With the Newts" is a satirical novel by this Czech dramatist who coined the word "robot."',
+  answer:'Karel Capek',
+  wrongAnswer1:'Stanislaw Lem',
+  wrongAnswer2:'Boris Badenov',
+  wrongAnswer3:'Vaclav Havel',
+}
+var sfq3 = {
+  clue:'Credited with formulating the "Three Laws of Robotics", he said the idea came from his editor John Campbell.',
+  answer:'Isaac Asimov',
+  wrongAnswer1:'Ray Bradbury',
+  wrongAnswer2:'Rob Bott',
+  wrongAnswer3:'H.G. Wells',
+}
+var sfq4 = {
+  clue:'He won 6 Hugo Awards for his fiction, including "Starship Troopers" & "Stranger in a Strange Land."',
+  answer:'Robert Heinlein',
+  wrongAnswer1:'Frank Herbert',
+  wrongAnswer2:'Gene Wolfe',
+  wrongAnswer3:'Lee Ving',
+}
+var sfq5 = {
+  clue:'This 1898 novel is divided into 2 parts: "The Coming of the Martians" & "The Earth Under the Martians."',
+  answer:'<em>War of the Worlds</em>',
+  wrongAnswer1:'<em>20,000 Leagues Under the Sea</em>',
+  wrongAnswer2:'<em>Sense and Sensibility</em>',
+  wrongAnswer3:'<em>Mars Attacks</em>',
+}
+var sfq6 = {
+  clue:'In "The Mysterious Island", by Jules Verne, this reclusive captain dies & is buried at sea in his submarine',
+  answer:'Capain Nemo',
+  wrongAnswer1:'Captain Hook',
+  wrongAnswer2:'Captain Crunch',
+  wrongAnswer3:'Captain Beefheart',
+}
+var sfq7 = {
+  clue:'This 1969 novella by Harlan Ellison tells the story of Blood, a telepathic canine who teaches his master to read.',
+  answer:'<em>A Boy and His Dog</em>',
+  wrongAnswer1:'<em>Old Yeller</em>',
+  wrongAnswer2:'<em>Tele-Pooch</em>',
+  wrongAnswer3:'<em>Blood</em>',
+}
+var sfq8 = {
+  clue:'Max Brooks left us wanting more braaaains with "World War Z", an oral history of the war versus them.',
+  answer:'Zombies',
+  wrongAnswer1:'Evil scientists',
+  wrongAnswer2:'Martians',
+  wrongAnswer3:'Newts',
+}
+var sfq9 = {
+  clue:'The central character in this 1953 novel by Ray Bradbury is third-generation fireman Guy Montag.',
+  answer:'<em>Fahrenheit 451</em>',
+  wrongAnswer1:'<em>The Illustrated Man</em>',
+  wrongAnswer2:'<em>On the Road</em>',
+  wrongAnswer3:'<em>Jesus on Mars</em>',
+}
+var sfq10 = {
+  clue:'"American Gods" is a Hugo Award-winning novel by this creator of the "Sandman" comic.',
+  answer:'Neil Gaiman',
+  wrongAnswer1:'Alan Moore',
+  wrongAnswer2:'Grant Morrison',
+  wrongAnswer3:'Morpheus',
+}
