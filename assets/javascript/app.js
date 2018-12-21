cb8518d0934f0d48dbeb5910be849355368d2968
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2018-12-21 13:57:22 -0500

    long in-class session, got timer to work, answer flow.  Mostly done, except for modal informing user of correct answer in cases where they answered wrong or ran out of time.  Still need to change so that the correct answer is not always the first of multiple choice options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,16 +7,14 @@ console.log(questions);
 
 // setting variables
 questionsHTM = $("#question");
+answer1HTM = $("#answer1");
+answer2HTM = $("#answer2");
+answer3HTM = $("#answer3");
+answer4HTM = $("#answer4");
+questionsCorrectHTM = $("#modal-number-correct");
 
 //setting up randomized order questions array
 var randomOrderQuestions = [];
-
-// variable to hold setInterval running timer
-var intervalId;
-
-// prevents the clock from being sped up unnecessarily
-var clockRunning = false;
-var time = 0;
 
 // setting up while loop to create a new array of questions in a random order
   while (randomOrderQuestions.length<10) {
@@ -35,6 +33,23 @@ console.log("random order questions:");
 console.log(randomOrderQuestions);
 console.log("final questions:");
 console.log(questions);
+
+// variable to keep track of which question/answer object program is on
+QAObject = 0;
+
+// variable to hold number of questions correct
+questionsCorrect = 0;
+
+// variable to keep track of whether last question was true or false;
+lastQuestionWas = false;
+
+// variable to hold setInterval running timer
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+var time = 0;
+
 
 // functions
 function reset() {
@@ -69,17 +84,19 @@ function count() {
 
   // display converted time on page
   $("#display").text(converted);
-
-  if (time>30){
+  if (time==30){
     stop();
     reset();
     $("#modal2").addClass("is-active");
     $("#continue2").click(function(){
     $(".modal").removeClass("is-active");
-    // need to put reference to function to put up new question and start timer again
-    // start();
 
-  });
+    lastQuestionWas = false;
+    // reference to function to put up new question and start timer again
+    
+    NextQuestion();
+    
+    });
 
   }
 }
@@ -101,35 +118,84 @@ function timeConverter(t) {
   }
 
   return minutes + ":" + seconds;
-}
 
+}
+// doing this seems to be necessary to keep timer from getting messed up after reloading
 stop();
 reset();
-//setting get ready modal and waiting for response to continue
 
 
 
-// looping questions
-for (i=0; i<10; i++) {
-  
-
-  questionsHTM.text(randomOrderQuestions[i].clue);
+function NextQuestion() {
+  if (clockRunning===false){
+    QAObject++;
+    if (QAObject>9){
+      stop();
+      reset();
+      gameEnd();
+    }
+    else {
+      questionsHTM.text(randomOrderQuestions[QAObject].clue);
+      console.log("Question/answer object: "+QAObject);
+      answer1HTM.html(randomOrderQuestions[QAObject].answer);
+      answer2HTM.html(randomOrderQuestions[QAObject].wrongAnswer1);
+      answer3HTM.html(randomOrderQuestions[QAObject].wrongAnswer2);
+      answer4HTM.html(randomOrderQuestions[QAObject].wrongAnswer3);
+      start();
+    }
+  }
 }
 
+function gameEnd() {
+  $(questionsCorrectHTM).text(questionsCorrect);
+  $("#modal3").addClass("is-active");
+}
 
   
-  $("#modal1").addClass("is-active");
+  // putting initial question into html
+  questionsHTM.text(randomOrderQuestions[QAObject].clue);
+  answer1HTM.html(randomOrderQuestions[QAObject].answer);
+  answer2HTM.html(randomOrderQuestions[QAObject].wrongAnswer1);
+  answer3HTM.html(randomOrderQuestions[QAObject].wrongAnswer2);
+  answer4HTM.html(randomOrderQuestions[QAObject].wrongAnswer3);
 
+  // putting initial answers into html *******************
+
+
+
+
+  //setting get ready modal to pop up on page load
+  $("#modal1").addClass("is-active");
+  // setting modal okay button to close and start timer
   $("#continue").click(function(){
     $("#modal1").removeClass("is-active");
     start();
   });
 
-
+// Setting up event listener for submit button
+$("#submit").click(function(){
+  event.preventDefault();
+  if ($("input[value='a']:checked").val()) {
+    questionsCorrect++;
+    lastQuestionWas = true;
+    alert("you got that correct");
+    stop();
+    reset();
+    NextQuestion();
+  }
+  else {
+    lastQuestionWas = false;
+    alert("you got that wrong");
+    stop();
+    reset();
+    NextQuestion();
+  }
+});
   
 
-  // else if user submits right answer, give +1 to score
+
 
 stop();
 reset();
 // game over page, displaying final score.
+
