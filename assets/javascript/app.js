1f980c773d022d2d62e2d80beb110c6ebb3103f8
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2018-12-20 22:46:16 -0500

    tried to work in timer and modals, with some limited success

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,13 @@ questionsHTM = $("#question");
 //setting up randomized order questions array
 var randomOrderQuestions = [];
 
+// variable to hold setInterval running timer
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+var time = 0;
+
 // setting up while loop to create a new array of questions in a random order
   while (randomOrderQuestions.length<10) {
     // setting up random index # from (remaining) questions array
@@ -29,24 +36,100 @@ console.log(randomOrderQuestions);
 console.log("final questions:");
 console.log(questions);
 
+// functions
+function reset() {
+
+  time = 0;
+
+  // reset display div
+  $("#display").text("00:00");
+
+}
+function start() {
+
+  // DONE: Use setInterval to start the count here and set the clock to running.
+  if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
+  }
+}
+function stop() {
+
+  // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+  clearInterval(intervalId);
+  clockRunning = false;
+}
+function count() {
+
+  time++;
+
+  // Get the current time, pass that into the timeConverter function,and save the result in a variable.
+  var converted = timeConverter(time);
+  console.log(converted);
+
+  // display converted time on page
+  $("#display").text(converted);
+
+  if (time>30){
+    stop();
+    reset();
+    $("#modal2").addClass("is-active");
+    $("#continue2").click(function(){
+    $(".modal").removeClass("is-active");
+    // need to put reference to function to put up new question and start timer again
+    // start();
+
+  });
+
+  }
+}
+// will not need minutes for this, will remove unnecessary code later, as i swiped this from a class activity and don't want to mess with it just yet since it is working
+function timeConverter(t) {
+
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+stop();
+reset();
+//setting get ready modal and waiting for response to continue
 
 
-//setting get-ready modal, wait for response
 
 // looping questions
 for (i=0; i<10; i++) {
-  // set timer variable and start running
-  // clear timer, set timer
+  
 
-  // populating question at this iteration
   questionsHTM.text(randomOrderQuestions[i].clue);
+}
 
-  // populating answers at this iterations
 
-  // if timer runs out, function to give correct answer and move on
+  
+  $("#modal1").addClass("is-active");
+
+  $("#continue").click(function(){
+    $("#modal1").removeClass("is-active");
+    start();
+  });
+
+
+  
 
   // else if user submits right answer, give +1 to score
 
-}
-
+stop();
+reset();
 // game over page, displaying final score.
